60b97c56f383ca8d839e6719cabe1ec778afe1d9
Mohau Mushi <david1mushi1@gmail.com>
2024-03-11 19:48:30 +0200

    added validation logic code

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function validateSyntax() {
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
         
-        
+        if(/^pet_[a-zA-Z0-9]+$/.test(input)){
+            result = "Valid Syntax 🟢";
+         } else {
+            result = "Invalid Syntax 🔴";
+         } 
+
             document.getElementById('result').innerText = result;
     }
