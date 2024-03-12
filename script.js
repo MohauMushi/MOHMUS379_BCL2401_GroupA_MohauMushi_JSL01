02d050a5c67950c46a38b8d94769194cc50334fb
Mohau Mushi <david1mushi1@gmail.com>
2024-03-12 09:42:41 +0200

    commented validation logic

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ function validateSyntax() {
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
         
+        /*The code below checks if the input follows a specific syntax pattern starting with "pet_". 
+        If it does, it sets the result variable to "Valid Syntax 🟢", otherwise it sets it to "Invalid Syntax 🔴" */
         if(/^pet_[a-zA-Z0-9]+$/.test(input)){
             result = "Valid Syntax 🟢";
          } else {
             result = "Invalid Syntax 🔴";
          } 
-
+           
+         /*This code sets the text content of the HTML element with the id "result" to the value of the result variable. 
+         The updated text will be displayed in the paragraph element on the webpage */
             document.getElementById('result').innerText = result;
     }
